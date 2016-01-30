225da95c1300ee8a0968111935e549685ccf616a
Barry Waluszko <barry@glanton.com>
2016-01-30 11:28:41 +0100

    Added options to switch on/off draggable feature for ngDialog

--- js/ngDialog.js
+++ js/ngDialog.js
@@ -54,6 +54,7 @@
             cache: true,
             trapFocus: true,
             preserveFocus: true,
+            draggable: false,
             ariaAuto: true,
             ariaRole: null,
             ariaLabelledById: null,
@@ -440,6 +441,10 @@
                             return '$stateChangeSuccess';
                         }
                         return '$locationChangeSuccess';
+                    },
+
+                    dragClassName: function () {
+                        return 'drag-header';
                     }
                 };
 
@@ -459,6 +464,7 @@
                      * - closeByEscape {Boolean} - default true
                      * - closeByDocument {Boolean} - default true
                      * - preCloseCallback {String|Function} - user supplied function name/function called before closing dialog (if set)
+                     * - draggable {Boolean} - move the ngDialog object by clicking on it's header with the mouse and dragging it anywhere within the viewport (if enabled)
                      * @return {Object} dialog
                      */
                     open: function (opts) {
@@ -501,7 +507,14 @@
                             if (options.showClose) {
                                 template += '<div class="ngdialog-close"></div>';
                             }
-
+                            
+                            //if draggable functionality is enabled
+                            if (options.draggable) {
+                                //check if template contains draggable container (like: <div class='header'></div>)
+                                var hasContainer = getByClass($el(template), privateMethods.dragClassName());                                
+                                if(hasContainer===null)
+                                    template = '<div class="' + privateMethods.dragClassName()  + '">Drag me</div>' + template; //add draggable container
+                            }
                             var hasOverlayClass = options.overlay ? '' : ' ngdialog-no-overlay';
                             $dialog = $el('<div id="'+dialogID + '" class="ngdialog' + hasOverlayClass + '"></div>');
                             $dialog.html((options.overlay ?
@@ -649,49 +662,30 @@
 
                             dialogsCount += 1;
 
-                            ////DGRAGGABLE BEGIN
-                            if (true) {
-                                var elHeader = getElemByClass($dialog, 'header');
-                                var elDialogContent = getElemByClass($dialog, 'ngdialog-content');
-                                if (elHeader && elHeader != null && elDialogContent && elDialogContent != null) {
-                                    console.log($dialog);
+                            if (options.draggable) {
+                                var elHeader = getByClass($dialog, privateMethods.dragClassName()); //check if in template already exist element that will be used to drag
+                                var elDialogContent = getByClass($dialog, 'ngdialog-content');
+                                if (elHeader !== null && elDialogContent !== null) {
                                     var startX = 0, startY = 0;
                                     var x = 0, y = 0;
-
-                                    console.log(elDialogContent);
-
-                                    elDialogContent.css({
-                                        position: 'relative'
-                                    });
-
                                     elHeader.css({
                                         cursor: 'move' //'pointer' 
                                     });
 
-                                    elHeader.on('mousedown', function (event) {
-                                        //Prevent default dragging of selected content.
-                                        //Not used here since this disables input fields.
-                                        //event.preventDefault();
+                                    elHeader.on('mousedown', function (event) {                                       
                                         startX = event.screenX - x;
                                         startY = event.screenY - y;
                                         $document.on('mousemove', mousemove);
                                         $document.on('mouseup', mouseup);
                                     });
 
-                                    function mousemove(ev) {
-                                        //Flag passed from input elements: ng-focus="setDrag(true)" ng-blur="setDrag(false)".
+                                    function mousemove(ev) {           
                                         y = ev.screenY - startY;
                                         x = ev.screenX - startX;
                                         elDialogContent.css({
                                             top: y + 'px',
                                             left: x + 'px'
-                                        });
-                                        //element[0].focus();
-
-                                        //Flag set for disabling vertical re-center after any dragging.
-                                        //hasDragged = true;
-
-                                        //Disable element/text selection.
+                                        });                                       
                                         window.getSelection().removeAllRanges();
                                     }
 
@@ -699,21 +693,6 @@
                                         $document.unbind('mousemove', mousemove);
                                         $document.unbind('mouseup', mouseup);
                                     }
-                                }
-
-                                function getElemByClass(elem, headerClass) {
-                                    var elHeader = null;
-                                    if (elem && elem != null) {
-                                        var divList = angular.element(elem.find('div'));
-                                        if (divList && divList != null) {
-                                            for (var i = 0; i < divList.length; i++) {
-                                                if (angular.element(divList[i]).hasClass(headerClass))
-                                                    elHeader = angular.element(divList[i]);
-                                            }
-                                        }
-                                    }
-
-                                    return elHeader;
                                 }
                             }
 
@@ -751,6 +730,20 @@
                             }
 
                             return loadTemplateUrl(tmpl, {cache: $templateCache});
+                        }
+
+                        function getByClass(elem, className) {
+                            var elemWithClass = null;
+                            if (elem !== null) {
+                                var divList = $el(elem.find('div'));
+                                if (divList !== null) {
+                                    for (var i = 0; i < divList.length; i++) {
+                                        if ($el(divList[i]).hasClass(className))
+                                            elemWithClass = $el(divList[i]);
+                                    }
+                                }
+                            }
+                            return elemWithClass;
                         }
                     },
 
